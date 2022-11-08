1c2478668005dfbbff041af759c5fe7a0c1bbfb3
Betül Iskender <betul.iskender@outlook.dk>
2022-11-08 09:34:57 +0100

    user reource og admin resource lavet

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -32,6 +32,8 @@ public class ApplicationConfig extends Application {
         resources.add(security.errorhandling.AuthenticationExceptionMapper.class);
         resources.add(security.errorhandling.NotAuthorizedExceptionMapper.class);
         resources.add(MealResource.class);
+        resources.add(AdminResource.class);
+        resources.add(UserResource.class);
     }
     
 }
